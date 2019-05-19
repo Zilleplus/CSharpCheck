6898d6c6aed75b71066d613f877ca06f159d833e
Willem Melis <willem.melis@student.kuleuven.be>
2019-05-19 22:25:29 +0200

    added simple demo of == draft impl

---
+++ Check/Check/AssertExtension.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Xunit;
+
+namespace Check
+{
+    public static class AssertExtension
+    {
+        public static void Check<T>(
+            this Expression<Predicate<T>> expression,
+            T value)
+        {
+            // check if is either == , equals, or random expression
+            switch (expression.Body)
+            {
+                case BinaryExpression be:
+                    ParseBinaryExpression<T>(be,value);
+                    break;
+                default:
+                    Assert.True(false);
+                    break;
+            }
+        }
+
+        private static void ParseBinaryExpression<T>(BinaryExpression be, T value)
+        {
+            switch (be.NodeType)
+            {
+                case ExpressionType.Add:
+                    break;
+                case ExpressionType.AddAssign:
+                    break;
+                case ExpressionType.AddAssignChecked:
+                    break;
+                case ExpressionType.AddChecked:
+                    break;
+                case ExpressionType.And:
+                    break;
+                case ExpressionType.AndAlso:
+                    break;
+                case ExpressionType.AndAssign:
+                    break;
+                case ExpressionType.ArrayIndex:
+                    break;
+                case ExpressionType.ArrayLength:
+                    break;
+                case ExpressionType.Assign:
+                    break;
+                case ExpressionType.Block:
+                    break;
+                case ExpressionType.Call:
+                    break;
+                case ExpressionType.Coalesce:
+                    break;
+                case ExpressionType.Conditional:
+                    break;
+                case ExpressionType.Constant:
+                    break;
+                case ExpressionType.Convert:
+                    break;
+                case ExpressionType.ConvertChecked:
+                    break;
+                case ExpressionType.DebugInfo:
+                    break;
+                case ExpressionType.Decrement:
+                    break;
+                case ExpressionType.Default:
+                    break;
+                case ExpressionType.Divide:
+                    break;
+                case ExpressionType.DivideAssign:
+                    break;
+                case ExpressionType.Dynamic:
+                    break;
+                case ExpressionType.Equal:
+                    var left = be.Left;
+                    var right = be.Right;
+
+                    if (be.Left is ConstantExpression lce)
+                    {
+                        Assert.Equal(lce.Value, value);
+                    }
+                    else if (be.Right is ConstantExpression rce)
+                    {
+                        Assert.Equal(rce.Value, value);
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
+
+                    break;
+                case ExpressionType.ExclusiveOr:
+                    break;
+                case ExpressionType.ExclusiveOrAssign:
+                    break;
+                case ExpressionType.Extension:
+                    break;
+                case ExpressionType.Goto:
+                    break;
+                case ExpressionType.GreaterThan:
+                    break;
+                case ExpressionType.GreaterThanOrEqual:
+                    break;
+                case ExpressionType.Increment:
+                    break;
+                case ExpressionType.Index:
+                    break;
+                case ExpressionType.Invoke:
+                    break;
+                case ExpressionType.IsFalse:
+                    break;
+                case ExpressionType.IsTrue:
+                    break;
+                case ExpressionType.Label:
+                    break;
+                case ExpressionType.Lambda:
+                    break;
+                case ExpressionType.LeftShift:
+                    break;
+                case ExpressionType.LeftShiftAssign:
+                    break;
+                case ExpressionType.LessThan:
+                    break;
+                case ExpressionType.LessThanOrEqual:
+                    break;
+                case ExpressionType.ListInit:
+                    break;
+                case ExpressionType.Loop:
+                    break;
+                case ExpressionType.MemberAccess:
+                    break;
+                case ExpressionType.MemberInit:
+                    break;
+                case ExpressionType.Modulo:
+                    break;
+                case ExpressionType.ModuloAssign:
+                    break;
+                case ExpressionType.Multiply:
+                    break;
+                case ExpressionType.MultiplyAssign:
+                    break;
+                case ExpressionType.MultiplyAssignChecked:
+                    break;
+                case ExpressionType.MultiplyChecked:
+                    break;
+                case ExpressionType.Negate:
+                    break;
+                case ExpressionType.NegateChecked:
+                    break;
+                case ExpressionType.New:
+                    break;
+                case ExpressionType.NewArrayBounds:
+                    break;
+                case ExpressionType.NewArrayInit:
+                    break;
+                case ExpressionType.Not:
+                    break;
+                case ExpressionType.NotEqual:
+                    break;
+                case ExpressionType.OnesComplement:
+                    break;
+                case ExpressionType.Or:
+                    break;
+                case ExpressionType.OrAssign:
+                    break;
+                case ExpressionType.OrElse:
+                    break;
+                case ExpressionType.Parameter:
+                    break;
+                case ExpressionType.PostDecrementAssign:
+                    break;
+                case ExpressionType.PostIncrementAssign:
+                    break;
+                case ExpressionType.Power:
+                    break;
+                case ExpressionType.PowerAssign:
+                    break;
+                case ExpressionType.PreDecrementAssign:
+                    break;
+                case ExpressionType.PreIncrementAssign:
+                    break;
+                case ExpressionType.Quote:
+                    break;
+                case ExpressionType.RightShift:
+                    break;
+                case ExpressionType.RightShiftAssign:
+                    break;
+                case ExpressionType.RuntimeVariables:
+                    break;
+                case ExpressionType.Subtract:
+                    break;
+                case ExpressionType.SubtractAssign:
+                    break;
+                case ExpressionType.SubtractAssignChecked:
+                    break;
+                case ExpressionType.SubtractChecked:
+                    break;
+                case ExpressionType.Switch:
+                    break;
+                case ExpressionType.Throw:
+                    break;
+                case ExpressionType.Try:
+                    break;
+                case ExpressionType.TypeAs:
+                    break;
+                case ExpressionType.TypeEqual:
+                    break;
+                case ExpressionType.TypeIs:
+                    break;
+                case ExpressionType.UnaryPlus:
+                    break;
+                case ExpressionType.Unbox:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Check/Check/SimpleTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Text;
+using Xunit;
+
+namespace Check
+{
+    public class SimpleTests
+    {
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Given_Number_Check_Sum(
+            string testName,
+            double a,
+            double b,
+            Expression<Predicate<double>> expr)
+        {
+            Debug.Write(testName);
+
+            var c = a + b;
+
+            expr.Check(c);
+        }
+
+        public static TheoryData<string, double, double, Expression<Predicate<double>>> GetData()
+        {
+            return new TheoryData<string, double, double, Expression<Predicate<double>>>
+            {
+                {
+                    "test1",
+                    1,
+                    2,
+                    (x)=>x==4
+                },
+                //{
+                //    "test2",
+                //    1,
+                //    2,
+                //    (x)=>x.Equals(3)
+                //}
+            };
+        }
+    }
+}
